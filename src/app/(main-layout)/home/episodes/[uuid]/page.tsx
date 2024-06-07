282aacd0f8ed9d5635004e53494facbffbaad8dd
VuLH <lehoangvu@bacoor.co>
2024-06-07 09:53:00 +0700

    feat: add metadata title

--- src/app/(main-layout)/home/episodes/[uuid]/page.tsx
+++ src/app/(main-layout)/home/episodes/[uuid]/page.tsx
@@ -1,6 +1,24 @@
 import EpisodesSevice from "@/services/episodes.service";
 import EpisodeDetailsView from "./view";
+import { Metadata } from "next";
 
+type Props = {
+  params: { uuid: string };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const response = await EpisodesSevice.GetEpisodeDetails(params.uuid);
+  let title = "New Podcast";
+  let description = "My podcast description";
+  if (response.status === "success") {
+    title = response.data.episode_details.episode_name;
+    description = response.data.episode_details.episode_desc;
+  }
+  return {
+    title,
+    description,
+  };
+}
 const EpisodeDetailsPage = async ({ params }: { params: { uuid: string } }) => {
   const response = await EpisodesSevice.GetEpisodeDetails(params.uuid);
   if (response.status === "fail") return <h1>Not found...</h1>;
